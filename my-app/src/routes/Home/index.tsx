5d51856673d136c23592d5fb9bb885d80f66b495
Muale-0 <murilomartinscampos26@gmail.com>
2026-09-11 09:59:47 -0300

    feat:uso de useState useEfect useParamns

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 
 export default function Home() {
   document.title = "Home";
+  const[clicado,Setclicado] = useState<number>(0);
+
+  useEffect( ()=>{
+    console.log("Um Milhão de linhas sendo carregadas:", clicado);
+  },[clicado] );
+  
   return (
     <main>
         <h2>Home</h2>
+        <div>
+          <p>Valor do STATE : {clicado}</p>
+          <button onClick={()=> Setclicado(clicado + 1)}>ALTERAR VALOR = {clicado}</button>
+        </div>
     </main>
   )
 }
