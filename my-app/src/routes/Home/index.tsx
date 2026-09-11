98d03a0438ab31a91117b6607a21e2a3f82cf6f1
carolinakotz <carolkotz@gmail.com>
2026-09-11 09:58:24 -0300

    feat: Exemplo de rotas dinâmicas.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
 
     document.title = "Home";
 
+    const [clicado, setClicado] = useState<number>(0);
+
+    useEffect(() => { console.log("1 milhão de linhas sendo carregadas: ", clicado) }, [clicado]);
+
     return (
         <main>
             <h2>Home</h2>
+            <div>
+                <p>Valor do State: {clicado}</p>
+                <button onClick={() => setClicado(clicado + 1)}>Alterar valor = {clicado}</button>
+            </div>
         </main>
     )
 }
