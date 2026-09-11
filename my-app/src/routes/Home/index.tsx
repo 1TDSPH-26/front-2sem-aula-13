505d9103f589fd8ca60a0d01b143fa43e9fcb422
Pedro-H-Salvatore <labsfiap@fiap.com.br>
2026-09-11 10:01:36 -0300

    feat: Utilizando os hooks para achar produtos pelo id, e atualizar o esado sem a necessidade de atualizar a página.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react"
 
 
 export default function Home() {
+    const[clicado, setClicado] = useState<number>(0)
+    function adicionar(){
+        if(clicado >= 10){            
+            setClicado(10)
+        }else{
+            setClicado(clicado + 1)
+        }
+        
 
+    }
 
+    function diminuir() {
+        if(clicado <=0){
+            setClicado(0)
+        }else{
+            setClicado(clicado - 1)
+        }
+        
+    }
+
+    useEffect(() => { console.log(clicado) },[] )
+    
     return(
         <div>
             <h1>Home </h1>
+            <div>  
+                <p>Valor de State: {clicado}</p>
+                <p>{clicado >= 10 ? "Limite Maximo de 10": ""}</p>
+                <p>{clicado <= 0 ? "Minímo de 0" : ""}</p>
+                <button onClick={() => adicionar()}>Alterar valor</button>
+                <button onClick={() => diminuir()}>diminuir</button>
+            </div>
         </div>
+    
     )
 }
