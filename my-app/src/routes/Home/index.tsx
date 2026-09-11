c3525373d131a43a2640cad5e45bd408aa273f7a
murilo-a-souza <sir.murilos@gmail.com>
2026-09-11 09:22:09 -0300

    feat: useState e useEffect.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+    document.title = "Home";
+
+    const[clicado,setClicado] = useState<number>(0)
+    const addClicado = () => {setClicado(clicado+1)}
+
+    useEffect(()=>{console.log("Um milhão de linhas sendo carregas: ", clicado)},[]);
+
     return (
         <main>
             <h2>Home</h2>
+            <div>
+                <p>Valor do STATE: {clicado}</p>
+                <button onClick={addClicado}>ALTERAR VALOR = {clicado}</button>
+            </div>
         </main>
     )
 }
